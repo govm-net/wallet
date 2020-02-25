f05fb749fa29492d7e44cee5370a7bc9eddae2d3
lengzhao <zgq_126@126.com>
2020-02-25 22:16:19 +0800

    update api

--- bin/html/routers.go
+++ bin/html/routers.go
@@ -67,10 +67,10 @@ var routes = Routes{
 	},
 
 	Route{
-		"TransactionTranferPost",
+		"TransactionTransferPost",
 		strings.ToUpper("Post"),
-		"/api/v1/{chain}/transaction/tranfer",
-		TransactionTranferPost,
+		"/api/v1/{chain}/transaction/transfer",
+		TransactionTransferPost,
 	},
 
 	Route{
