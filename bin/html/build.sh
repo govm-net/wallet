8a0c60aea8363aa88809d30cb24fc51be915db25
lengzhao <zgq_126@126.com>
2020-03-02 15:22:21 +0800

    support mac

--- bin/html/build.sh
+++ bin/html/build.sh
@@ -2,11 +2,12 @@
 
 CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o html_wallet_windows.exe
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o html_wallet_linux
+CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -o html_wallet_mac
 rm html_wallet -rf
-rm 
 mkdir html_wallet
 mv html_wallet_windows.exe html_wallet
 mv html_wallet_linux html_wallet
+mv html_wallet_mac html_wallet
 cp static html_wallet -rf
 tar zcvf html_wallet_$(date +'%y%m%d_%H%M%S').tar.gz html_wallet
 rm html_wallet -rf
